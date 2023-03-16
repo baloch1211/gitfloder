9975162ea41ee779581d1be3f3d57d4d6e261a51
sameer <sameerhabib72@gmail.com>
2023-03-16 15:43:13 +0500

    setting is not setting.php

---
+++ setting.php
@@ -0,0 +1,30 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<title>from page </title>
+	
+	<style type="text/css">
+		body{border: 10px solid transparent;
+             background-color: hsl(0, 100%, 90%);
+		}
+		span{color: red;}
+		input[type=submit] {
+  background-color: #04AA6D;
+  color: white;
+  padding: 12px 20px;
+  border: none;
+  border-radius: 4px;
+  cursor: pointer;
+  float: center;
+	input[type=submit]:hover 
+  background-color: #45a049;}
+  
+	</style>
+
+</head>
+
+<body>
+</body>
+</html>
